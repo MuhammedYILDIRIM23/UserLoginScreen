5074d3e4473092879ae3b3067a70d727e9aadaf6
Muhammed YILDIRIM <m.yldrmmm23@gmail.com>
2021-05-26 22:16:11 +0300

    Initial Commit

---
+++ RamazanUserInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RamazanUserInterface
+//
+//  Created by Muhammed YILDIRIM  on 26.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
